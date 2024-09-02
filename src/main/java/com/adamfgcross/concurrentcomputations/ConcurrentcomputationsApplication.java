0014a029a4bca7d12cd717fcb99053e4eaf7d1f5
Adam <adamfgcross@gmail.com>
2024-09-02 15:03:45 -0400

    create initial project outline

---
+++ src/main/java/com/adamfgcross/concurrentcomputations/ConcurrentcomputationsApplication.java
@@ -0,0 +1,13 @@
+package com.adamfgcross.concurrentcomputations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentcomputationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrentcomputationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adamfgcross/concurrentcomputations/ConcurrentcomputationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adamfgcross.concurrentcomputations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrentcomputationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
