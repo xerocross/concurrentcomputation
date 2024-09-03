bbe85ee6a1d531c3e663b4d405eb357373b39f11
Adam <adamfgcross@gmail.com>
2024-09-03 13:01:19 -0400

    create IntFactorService

---
+++ src/main/java/com/adamfgcross/concurrentcomputations/service/IntFactorService.java
@@ -0,0 +1,13 @@
+package com.adamfgcross.concurrentcomputations.service;
+
+import org.springframework.stereotype.Service;
+
+import com.adamfgcross.concurrentcomputations.dto.IntFactorTaskRequest;
+
+@Service
+public class IntFactorService {
+
+	public Long submitTask(IntFactorTaskRequest intFactorTaskRequest) {
+		return null;
+	}
+}
