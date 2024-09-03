4fd99a7aec2da89371de2a5b0bb8be7ba9b64384
Adam <adamfgcross@gmail.com>
2024-09-03 12:40:30 -0400

    create User entity

---
+++ src/main/java/com/adamfgcross/concurrentcomputations/domain/User.java
@@ -0,0 +1,54 @@
+package com.adamfgcross.concurrentcomputations.domain;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String username;
+	
+	private String tempKey;
+	
+	public String getTempKey() {
+		return tempKey;
+	}
+	public void setTempKey(String tempKey) {
+		this.tempKey = tempKey;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	@Override
+    public boolean equals(Object o) {
+        if (this == o) return true; // Check if they are the same instance
+        if (o == null || getClass() != o.getClass()) return false; // Check if o is null or not of the same class
+        User user = (User) o; // Cast o to User and compare relevant fields
+        return Objects.equals(id, user.id) &&
+               Objects.equals(username, user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username); // Generate hash code based on relevant fields
+    }
+	
+}
