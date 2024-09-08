21d2e6fc26edde3b264fe2d6eee0e0752f1348d5
Adam <adamfgcross@gmail.com>
2024-09-08 13:35:20 -0400

    add relevant data in primes in range response DTO

--- src/main/java/com/adamfgcross/concurrentcomputations/dto/PrimesInRangeResponse.java
+++ src/main/java/com/adamfgcross/concurrentcomputations/dto/PrimesInRangeResponse.java
@@ -1,5 +1,45 @@
 package com.adamfgcross.concurrentcomputations.dto;
 
-public class PrimesInRangeResponse extends TaskResponse {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.adamfgcross.concurrentcomputations.domain.PrimesInRangeTask;
+
+public class PrimesInRangeResponse extends TaskResponse {
+	
+	public PrimesInRangeResponse() {}
+	
+	public PrimesInRangeResponse(PrimesInRangeTask primesInRangeTask) {
+		this.setId(primesInRangeTask.getId());
+		this.setPrimes(primesInRangeTask.getPrimes().stream().toList());
+		this.setIsCompleted(primesInRangeTask.getIsCompleted());
+	}
+
+	public String getRangeMin() {
+		return rangeMin;
+	}
+
+	public void setRangeMin(String rangeMin) {
+		this.rangeMin = rangeMin;
+	}
+
+	public String getRangeMax() {
+		return rangeMax;
+	}
+
+	public void setRangeMax(String rangeMax) {
+		this.rangeMax = rangeMax;
+	}
+
+	private String rangeMin;
+	private String rangeMax;
+	private List<String> primes = new ArrayList<>();
+
+	public List<String> getPrimes() {
+		return primes;
+	}
+
+	public void setPrimes(List<String> primes) {
+		this.primes = primes;
+	}
 }
