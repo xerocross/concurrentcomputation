68bdf52c88d0192f59af20fc20ae55f7399b738a
Adam <adamfgcross@gmail.com>
2024-09-08 13:35:47 -0400

    add user to task response object

--- src/main/java/com/adamfgcross/concurrentcomputations/dto/TaskResponse.java
+++ src/main/java/com/adamfgcross/concurrentcomputations/dto/TaskResponse.java
@@ -4,7 +4,16 @@ public class TaskResponse {
 	private Long id;
 
 	private Boolean isCompleted = false;
+	private String username;
 	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
 	public Boolean getIsCompleted() {
 		return isCompleted;
 	}
