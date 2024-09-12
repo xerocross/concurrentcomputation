9dadd8aaa45a649f6be30784b038139a8c2048c4
Adam <adamfgcross@gmail.com>
2024-09-11 20:46:23 -0400

    create TaskStoreService to keep a reference to task futures associated with a task entity

---
+++ src/main/java/com/adamfgcross/concurrentcomputations/service/TaskStoreService.java
@@ -0,0 +1,34 @@
+package com.adamfgcross.concurrentcomputations.service;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Future;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class TaskStoreService {
+
+	private ConcurrentHashMap<Long, Set<Future<?>>> taskFutureMap;
+	
+	public TaskStoreService() {
+		taskFutureMap = new ConcurrentHashMap<>();
+	}
+	
+	public void storeTaskFuture(Long taskId, Future<?> future) {
+		taskFutureMap.computeIfAbsent(taskId, k -> ConcurrentHashMap.newKeySet()).add(future);
+	}
+	
+	public Optional<Set<Future<?>>> getTaskFutures(Long taskId) {
+		return Optional.ofNullable(taskFutureMap.get(taskId));
+	}
+	
+	public void removeFuture(Long taskId, Future<?> future) {
+		taskFutureMap.computeIfPresent(taskId,(key, futures) -> {
+			futures.remove(future);
+			return futures;
+		});
+	}
+}
