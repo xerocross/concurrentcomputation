5029e29d350ccc972703512ce5d48a17b02b6e3b
Adam <adamfgcross@gmail.com>
2024-09-27 17:29:40 -0400

    add EnableTransactionManagement

--- src/main/java/com/adamfgcross/concurrentcomputations/ConcurrentcomputationsApplication.java
+++ src/main/java/com/adamfgcross/concurrentcomputations/ConcurrentcomputationsApplication.java
@@ -2,8 +2,10 @@ package com.adamfgcross.concurrentcomputations;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class ConcurrentcomputationsApplication {
 
 	public static void main(String[] args) {
